a0127050628c671f9f30f3da418d6be14dbd821f
Mahesh <maheshgande5@gmail.com>
2024-06-24 01:10:11 +0530

    Added HTML and CSS page

---
+++ script.js
@@ -0,0 +1,10 @@
+const addBtn = document.querySelector("#addBtn");
+const main = documnet.querySelector("#main");
+
+addBtn.addEventListener("click", function() {
+    addNote();
+})
+
+const addNote = (text = "", title = "") => {
+    
+}
